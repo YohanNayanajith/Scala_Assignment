b1185d86df7b286202ac1b16247cde0d7c9a8339
root <yohannayanajith40@gmail.com>
2021-05-13 10:45:09 +0000

    Final

---
+++ Question1/19001088_q1.scala
@@ -0,0 +1,6 @@
+object Fahrenheit extends App{
+  def Convert(x:Double):Double = x*1.8+32
+
+  println(Convert(35))
+
+}
---
+++ Question2/sphere.scala
@@ -0,0 +1,6 @@
+object sphere extends App {
+  def cal_volume(radius:Double):Double = (4/3)*3.14*radius*radius*radius
+
+  println(cal_volume(5))
+
+}
